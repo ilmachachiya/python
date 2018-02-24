529208775c3adfbefad1cab5b7c062cb6d8cb3e5
ilmachachiya <35477350+ilmachachiya@users.noreply.github.com>
2018-02-24 09:25:15 +0530

    Add files via upload

---
+++ program range.py
@@ -0,0 +1,4 @@
+#program_range
+l=[5,8,9]
+for i in range(5,8,9):
+    print(i)
---
+++ string.py
@@ -0,0 +1,7 @@
+#using strings
+password="ilma@123"
+a=input("enter gmail id password")
+if a==password:
+    print("password correct")
+else:
+    print("the password is incorrect")
